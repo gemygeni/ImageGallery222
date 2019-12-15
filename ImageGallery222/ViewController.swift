9eecb52f60a64d36d49946bb38530693195a04ed
Ahmed Gamal <ahmedgamal.miner@gmail.com>
2019-12-16 01:16:23 +0200

    Initial Commit

---
+++ ImageGallery222/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery222
+//
+//  Created by AHMED GAMAL  on 12/16/19.
+//  Copyright © 2019 AHMED GAMAL . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
